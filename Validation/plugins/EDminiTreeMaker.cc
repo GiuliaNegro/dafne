364d3f046e3b7c0b518b2191034cb0e694df5d3d
GiuliaNegro <giulia.negro@cern.ch>
2017-03-09 18:25:33 +0100

    deleted old macros for DLDJ

--- Validation/plugins/EDminiTreeMaker.cc
+++
@@ -1,7 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "PhysicsTools/UtilAlgos/interface/EDAnalyzerWrapper.h"
-#include "dafne/Validation/interface/miniTreeMaker.h"
-
-typedef edm::AnalyzerWrapper<miniTreeMaker> EDminiTreeMaker;
-DEFINE_FWK_MODULE(EDminiTreeMaker);
