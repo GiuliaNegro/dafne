72163a123aa4f165d1d05c5a1cba5c233723b00e
GiuliaNegro <giulia.negro@cern.ch>
2017-04-27 19:43:23 +0200

    deleted macros to produce miniTrees only with singleObjects (to use flashgg microAOD for SingleEle)

--- Validation/plugins/EDminiTreeMaker_onlyEle.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-
-#include "PhysicsTools/UtilAlgos/interface/EDAnalyzerWrapper.h"
-#include "dafne/Validation/interface/miniTreeMaker_onlyEle.h"
-
-
-typedef edm::AnalyzerWrapper<miniTreeMaker_onlyEle> EDminiTreeMaker_onlyEle;
-DEFINE_FWK_MODULE(EDminiTreeMaker_onlyEle);
